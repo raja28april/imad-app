e2e8b729c3e5d5dfbde7953c50d6093cb7b4af59
Raja <30629518+raja28april@users.noreply.github.com>
2017-08-11 16:13:11 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req, res){
     //articleName = article-one
     //articles[articleName]={} content object for article one
-    var articleNAme = req.params.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]))
 });
 
