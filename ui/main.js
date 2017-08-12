7c03436a18a1f3e0b51a676311386f8b23f99ef0
Raja <30629518+raja28april@users.noreply.github.com>
2017-08-12 09:29:10 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'new value';
+
+//to move the image on click
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginRight ='100px';
+}
