976ebebebfbbe49603cb5f0bc5e0185a7e0687dd
Raja <30629518+raja28april@users.noreply.github.com>
2017-08-12 18:55:59 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ button.onclick = function(){
     
     //Render the vriable in the correct span
     
-    counter = counter +1;
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
