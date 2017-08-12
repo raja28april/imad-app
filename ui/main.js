23dd3abc45e60681f395156b5444112dc13a24f4
Raja <30629518+raja28april@users.noreply.github.com>
2017-08-12 18:56:41 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,6 +23,7 @@ img.onclick = function(){
 
 //Counter code
 var button =document.getElementById('counter');
+var counter = 0;
 button.onclick = function(){
     //make a request to the counter endpoint
     
